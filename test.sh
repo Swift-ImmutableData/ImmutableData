7176ad5751809fb78785e56c873bc38238b34e6a
Rick van Voorden <rick@northbronson.com>
2025-02-27 12:02:54 -0800

    update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -euox pipefail
 
 main() {
   toolchains="Xcode_16 Xcode_16.1 Xcode_16.2 Xcode_16.3_beta"
@@ -8,9 +8,10 @@ main() {
   for toolchain in $toolchains; do
     sudo xcode-select --switch /Applications/${toolchain}.app
     swift --version
-    swift package resolve
-    swift test
     swift package reset
+    swift package resolve
+    swift build
+    swift test
   done
   
   sudo xcode-select --reset
